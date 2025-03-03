279ad67bb6a845162309ee665a9f71abff1f43b0
PRINCE KUMAR PANKAJ <85887983+princekpankaj@users.noreply.github.com>
2025-03-03 10:36:10 +0530

    Time: 0 ms (100%), Space: 42 MB (7.66%) - LeetHub

--- 2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.java
+++ 2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.java
@@ -1,11 +1,14 @@
 class Solution {
     public char repeatedCharacter(String s) {
-        for (int i = 0; i < s.length(); i++ ) {
-            String subString = s.substring(0, i);
-            if (subString.contains(String.valueOf(s.charAt(i)))) {
-                return s.charAt(i);
-            }
+        int[] freq = new int[26]; 
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int index = c - 'a'; 
+            if(freq[index] != 0) 
+                return c; 
+            else 
+                freq[index]++; 
         }
-        return 'a';
+        return '\0'; 
     }
 }
