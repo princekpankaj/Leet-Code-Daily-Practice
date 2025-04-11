b5bc6f9910d41a034560b26fa0a00595b8e89723
PRINCE KUMAR PANKAJ <85887983+princekpankaj@users.noreply.github.com>
2025-04-11 14:30:47 +0530

    Time: 6 ms (98.01%), Space: 47.8 MB (82.57%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
+        
+        Map<String, List<String>> anagramMap = new HashMap<>();
+        
+        for (String str : strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
+            
+            if (!anagramMap.containsKey(sortedStr)) {
+                anagramMap.put(sortedStr, new ArrayList<>());
+            }
+            anagramMap.get(sortedStr).add(str);
+        }
+        
+        return new ArrayList<>(anagramMap.values());
+    }
+}
