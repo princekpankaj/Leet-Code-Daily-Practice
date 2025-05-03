bb8276211e0047e3ce57ead129d6849817aa4ba5
PRINCE KUMAR PANKAJ <85887983+princekpankaj@users.noreply.github.com>
2025-05-03 16:50:40 +0530

    Time: 272 ms (74.8%), Space: 0B (100%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,5 +1,16 @@
 # Write your MySQL query statement below
+
+-- with the help of GROPU BY and HAVING Clause
+
+-- SELECT name, population, area 
+-- FROM World
+-- GROUP BY name, population , area
+-- HAVING population >= 25000000 OR area >= 3000000;
+
+-- we can also do with he help of where clause
+
 SELECT name, population, area 
 FROM World
-GROUP BY name, population , area
-HAVING population >= 25000000 OR area >= 3000000;
+WHERE population >= 25000000 OR area >= 3000000;
+
+
